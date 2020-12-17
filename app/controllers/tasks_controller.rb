316dff4f07f3a1faa2ed702ecf6648ab2d108be6
minoru.sakamoto <minoru6432@gmail.com>
2020-12-17 06:14:14 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -12,13 +16,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'タスクを作成しました。'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'タスクの作成に失敗しました。'
       render :new
     end
   end
@@ -53,5 +57,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:status, :content)
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
 
-    get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
    
